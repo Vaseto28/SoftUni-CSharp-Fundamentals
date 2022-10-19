3cb181393de54de73eebadf0f9f5cd5a84a24794
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-19 21:48:35 +0300

    Completed task: T05.ForeignLanguages

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/foreign languages/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace foreign_languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var country = Console.ReadLine();
+            switch (country)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
