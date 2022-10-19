5af42f9ac647ac7e7ee100baf1160215f9f28f1d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-19 21:46:48 +0300

    Completed task: T01.BackInThirtyMinutes

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/back in 30 minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace back_in_30_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hours = int.Parse(Console.ReadLine());
+            var minutes = int.Parse(Console.ReadLine());
+            minutes += 30;
+            if (minutes >= 60)
+            {
+                minutes -= 60;
+                hours++;
+            }
+            if (hours >= 24)
+            {
+                hours -= 24;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
