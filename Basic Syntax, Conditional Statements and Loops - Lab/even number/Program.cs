2073d16a5cdc88ce88179f47a3ec276803ce195c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-19 21:48:12 +0300

    Completed task: T04.EvenNumber

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/even number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace even_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            while (number % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                if (number % 2 == 0)
+                {
+                    break;
+                }
+                number = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine($"The number is: {Math.Abs(number)}");
+        }
+    }
+}
