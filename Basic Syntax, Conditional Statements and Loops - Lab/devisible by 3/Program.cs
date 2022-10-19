01900bb76a8e8730de8db3e7e8bedf2152ebfa1c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-19 21:47:33 +0300

    Completed task: T03.DevisibleByThree

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/devisible by 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace devisible_by_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i < 100; i += 3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
