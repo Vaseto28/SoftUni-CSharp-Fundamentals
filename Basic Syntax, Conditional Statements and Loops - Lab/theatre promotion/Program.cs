d0730a5a14c21e3ea00a330bf5c0f75da159c322
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-24 09:11:20 +0300

    Completed task: T13.TheatrePromotion

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/theatre promotion/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace theatre_promotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typeOfDay = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var ticketPrice = 0.0;
+            if (age >= 0 && age <= 18)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 12;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 15;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 5;
+                        break;
+                }
+                Console.WriteLine($"{ticketPrice}$");
+                return;
+            }
+            else if (age > 18 && age <= 64)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 18;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 20;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 12;
+                        break;
+                }
+                Console.WriteLine($"{ticketPrice}$");
+                return;
+            }
+            else if (age > 64 && age <= 122)
+            {
+                switch (typeOfDay)
+                {
+                    case "Weekday":
+                        ticketPrice = 12;
+                        break;
+                    case "Weekend":
+                        ticketPrice = 15;
+                        break;
+                    case "Holiday":
+                        ticketPrice = 10;
+                        break;
+                }
+                Console.WriteLine($"{ticketPrice}$");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+        }
+    }
+}
