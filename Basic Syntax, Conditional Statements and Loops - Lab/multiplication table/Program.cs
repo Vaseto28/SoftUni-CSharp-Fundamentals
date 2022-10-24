9ae0288e23c2ca1b0a80439927abfda3722221bc
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-24 09:09:58 +0300

    Completed task: T09.MultiplicationTable

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/multiplication table/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace multiplication_table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+        }
+    }
+}
