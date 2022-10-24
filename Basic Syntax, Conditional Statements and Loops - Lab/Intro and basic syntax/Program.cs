12a8b9a79139ac4c3eee89c9a1ac5166d272cecf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-24 09:07:54 +0300

    Completed task: T06.IntroAndBasicSyntax

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/Intro and basic syntax/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Intro_and_basic_syntax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var grade = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:f2}");
+        }
+    }
+}
