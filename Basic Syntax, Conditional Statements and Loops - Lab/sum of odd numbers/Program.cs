f778af11307b635ab5ba73dc4a3af0f8b7606292
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-24 09:11:00 +0300

    Completed task: T12.SumOfOddNumbers

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/sum of odd numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sum_of_odd_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = 0; i < range; i++)
+            {
+                int oddNumber = 1 + (2 * i);
+                sum += oddNumber;
+                Console.WriteLine(oddNumber);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
