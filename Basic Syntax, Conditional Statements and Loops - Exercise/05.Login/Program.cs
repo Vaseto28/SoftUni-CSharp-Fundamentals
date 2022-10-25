b546ea4c68b4e741210a2f06d43fb5ba169f6cd3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-25 08:13:56 +0300

    Completed task: T05.Login

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/05.Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = Console.ReadLine();
+            var password = String.Empty;
+            for (int indexOfString = username.Length - 1; indexOfString >= 0; indexOfString--)
+            {
+                password += username[indexOfString];
+            }
+            for (int tries = 1; tries <= 4; tries++)
+            {
+                var enteredPassowrd = Console.ReadLine();
+                if (tries <= 4 && password == enteredPassowrd)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                if (tries < 4 && password != enteredPassowrd)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                    continue;
+                }
+                if (tries >= 4 && password != enteredPassowrd)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+            }
+        }
+    }
+}
