0ac19c821a94fccf9570866a56fbafd233068a3a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-25 08:13:42 +0300

    Completed task: T04.PrintAndSum

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/04. Print and Sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _04._Print_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = num1; i <= num2; i++)
+            {
+                Console.Write($"{i} ");
+                sum += i;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
