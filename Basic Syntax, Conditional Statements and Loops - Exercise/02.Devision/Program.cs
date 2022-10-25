62c53896c09e5ea5ba63f5a5138553006c03d716
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-25 08:13:01 +0300

    Completed task: T02.Devision

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/02.Devision/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.Devision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var flag = -1;
+            if (num % 10 == 0)
+            {
+                flag = 10;
+            }
+            else if (num % 7 == 0)
+            {
+                flag = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                flag = 6;
+            }
+            else if (num % 3 == 0)
+            {
+                flag = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                flag = 2;
+            }
+
+            if (flag == -1)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {flag}");
+            }
+        }
+    }
+}
