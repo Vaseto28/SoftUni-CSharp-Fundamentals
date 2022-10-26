1df90a45e52da0d38d87815aedc8740e5b2b9d63
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-26 11:08:46 +0300

    Completed task: T11.Orders

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/11.Orders/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = int.Parse(Console.ReadLine());
+            var finalPrice = 0.0;
+            var pricePerOrder = 0.0;
+            for (int i = 1; i <= orders; i++)
+            {
+                var pricePerCapsule = double.Parse(Console.ReadLine());
+                var days = int.Parse(Console.ReadLine());
+                var capsules = int.Parse(Console.ReadLine());
+                pricePerOrder = (days * capsules) * pricePerCapsule;
+                finalPrice += pricePerOrder;
+                Console.WriteLine($"The price for the coffee is: ${pricePerOrder:f2}");
+                pricePerOrder = 0.0;
+            }
+            Console.WriteLine($"Total: ${finalPrice:f2}");
+        }
+    }
+}
