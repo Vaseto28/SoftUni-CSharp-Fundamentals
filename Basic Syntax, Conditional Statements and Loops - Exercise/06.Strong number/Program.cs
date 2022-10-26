1811ca890734cd40e8fd417f500f51b8b5fe245a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-26 11:06:06 +0300

    Completed task: T06.StrongNumber

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/06.Strong number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _06.Strong_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Console.ReadLine();
+            var sumFact = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                var currentCharacter = num[i];
+                var currentDigit = (int)currentCharacter - 48;
+                var factorialOfTheNumber = 1;
+                for (int j = currentDigit; j > 1; j--)
+                {
+                    factorialOfTheNumber *= j;
+                }
+                sumFact += factorialOfTheNumber;
+            }
+            var num1 = int.Parse(num);
+            if (sumFact == num1)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
