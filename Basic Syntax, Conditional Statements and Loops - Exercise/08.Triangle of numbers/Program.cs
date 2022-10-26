766eaac9f12cdc2add1110f65edbaa01a4419d5c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-26 11:07:45 +0300

    Completed task: T08.TriangleOfNumbers

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/08.Triangle of numbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _08.Triangle_of_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int columns = 1; columns <= rows; columns++)
+                {
+                    Console.Write($"{rows} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
