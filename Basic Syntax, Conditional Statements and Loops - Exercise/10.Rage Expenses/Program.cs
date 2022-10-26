34773284124936dbc172030a0a4fc944e23e48f7
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-26 11:08:34 +0300

    Completed task: T10.RageExpences

---
+++ Basic Syntax, Conditional Statements and Loops - Exercise/10.Rage Expenses/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _10.Rage_Expenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lostGames = int.Parse(Console.ReadLine());
+            var headsetPrice = double.Parse(Console.ReadLine());
+            var mousePrice = double.Parse(Console.ReadLine());
+            var keyboardPrice = double.Parse(Console.ReadLine());
+            var displayPrice = double.Parse(Console.ReadLine());
+            var totalSum = (lostGames/2)*headsetPrice + (lostGames / 3) * mousePrice + (lostGames / 6) * keyboardPrice + (lostGames / 12) * displayPrice;
+            Console.WriteLine($"Rage expenses: {totalSum:f2} lv.");
+        }
+    }
+}
