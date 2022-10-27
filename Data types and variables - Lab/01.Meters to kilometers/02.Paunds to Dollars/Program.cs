339c3baed8f511fbe51faa3f9b2f8292fcaac625
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-27 13:04:56 +0300

    Completed task: T02.PaundsToDollars

---
+++ Data types and variables - Lab/01.Meters to kilometers/02.Paunds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02.Paunds_to_Dollars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var paunds = double.Parse(Console.ReadLine());
+            decimal dollars = (decimal)(paunds * 1.31);
+            Console.WriteLine($"{dollars:f3}");
+        }
+    }
+}
