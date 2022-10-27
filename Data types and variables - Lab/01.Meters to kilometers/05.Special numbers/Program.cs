c4d6b7b5c8e10e3f7d00512673fca02022a2ad45
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-27 13:06:00 +0300

    Completed task: T05.SpecialNumbers

---
+++ Data types and variables - Lab/01.Meters to kilometers/05.Special numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05.Special_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++)
+            {
+                var sum = 0;
+                var currentNumber = i;
+                while (currentNumber > 0)
+                {
+                    var currentLastNum = currentNumber % 10;
+                    sum += currentLastNum;
+                    currentNumber /= 10;
+                }
+                bool isSpecial = sum == 5 || sum == 7 || sum == 11;
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
