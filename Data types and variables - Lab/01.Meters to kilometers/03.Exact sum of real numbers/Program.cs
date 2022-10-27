63b9d5b38c7ebf5db524a4436ddcb41d84bcaf2e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-27 13:05:18 +0300

    Completed task: T03.ExactSumOfRealNumbers

---
+++ Data types and variables - Lab/01.Meters to kilometers/03.Exact sum of real numbers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _03.Exact_sum_of_real_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = int.Parse(Console.ReadLine());
+            var sum = (decimal)0.0;
+            for (int i = 0; i < numbers; i++)
+            {
+                var num = decimal.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
