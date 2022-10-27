0ff77a1f2c73c5e78d593d40c37734ce7fb45edd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-27 13:04:36 +0300

    Completed task: T01.MetersToKilometers

---
+++ Data types and variables - Lab/01.Meters to kilometers/01.Meters to kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.Meters_to_kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var meters = int.Parse(Console.ReadLine());
+            decimal kilometers = meters / 1000.0M;
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
