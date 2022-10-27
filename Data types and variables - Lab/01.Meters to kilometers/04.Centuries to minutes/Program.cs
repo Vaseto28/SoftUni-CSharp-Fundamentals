fefff667da542ebddcc9a481ff3deac595133d58
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-27 13:05:33 +0300

    Completed task: T04.CenturiesToMinutes

---
+++ Data types and variables - Lab/01.Meters to kilometers/04.Centuries to minutes/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04.Centuries_to_minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var centuries = short.Parse(Console.ReadLine());
+            var years = (int)(centuries * 100);
+            var days = (long)(years * 365.2422);
+            var hours = (long)(days * 24);
+            var minutes = (long)(hours * 60);
+            Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
