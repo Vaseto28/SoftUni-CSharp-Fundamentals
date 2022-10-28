ca20bd45ed0d4b0b17ef331cecf1419435de0b7c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:01:43 +0300

    Completed task: T07.ConcatNames

---
+++ Data types and variables - Lab/01.Meters to kilometers/07.Concat names/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _07.Concat_names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = Console.ReadLine();
+            var secondName = Console.ReadLine();
+            var delimeter = Console.ReadLine();
+            Console.WriteLine($"{name}{delimeter}{secondName}");
+        }
+    }
+}
