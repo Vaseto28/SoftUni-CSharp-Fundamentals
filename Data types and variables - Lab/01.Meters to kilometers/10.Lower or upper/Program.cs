47bfefee36e78753000c5dc18cb2fcd342ff374d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:03:01 +0300

    Completed task: T11.LowerOrUpper

---
+++ Data types and variables - Lab/01.Meters to kilometers/10.Lower or upper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _10.Lower_or_upper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letter = char.Parse(Console.ReadLine());
+            if ((int)letter >= 65 && (int)letter <= 90)
+            {
+                Console.WriteLine("upper-case");
+            }
+            else
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
