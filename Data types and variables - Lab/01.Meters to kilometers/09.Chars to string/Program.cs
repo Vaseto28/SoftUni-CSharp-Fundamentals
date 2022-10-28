77f56c4550a1de8359bb063ad26ed1418834eaa3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:02:10 +0300

    Completed task: T09.CharsToString

---
+++ Data types and variables - Lab/01.Meters to kilometers/09.Chars to string/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _09.Chars_to_string
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var char1 = char.Parse(Console.ReadLine());
+            var char2 = char.Parse(Console.ReadLine());
+            var char3 = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{char1}{char2}{char3}");
+        }
+    }
+}
