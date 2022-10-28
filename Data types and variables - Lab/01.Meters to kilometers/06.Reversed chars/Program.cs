12f848b93897c699d70be04b59c030f5b06e1ad6
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:01:23 +0300

    Completed task: T06.ReversedChars

---
+++ Data types and variables - Lab/01.Meters to kilometers/06.Reversed chars/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _06.Reversed_chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstChar = char.Parse(Console.ReadLine());
+            var secondChar = char.Parse(Console.ReadLine());
+            var thirdChar = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{thirdChar} {secondChar} {thirdChar}");
+        }
+    }
+}
