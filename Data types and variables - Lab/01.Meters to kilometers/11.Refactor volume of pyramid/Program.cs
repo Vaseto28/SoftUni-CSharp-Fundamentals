caad09f5be748f98300baf58883e683602e3ef12
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:02:34 +0300

    Completed task: T10.RefactorVolumeOfPyramid

---
+++ Data types and variables - Lab/01.Meters to kilometers/11.Refactor volume of pyramid/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _11.Refactor_volume_of_pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Length: ");
+            var length = double.Parse(Console.ReadLine());
+            Console.Write("Width: ");
+            var width = double.Parse(Console.ReadLine());
+            Console.Write("Height: ");
+            var height = double.Parse(Console.ReadLine());
+            var V = (length * width * height) / 3;
+            Console.Write($"Pyramid Volume: {V:f2}");
+
+        }
+    }
+}
