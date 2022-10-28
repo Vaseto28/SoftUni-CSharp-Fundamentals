6c3f8fc4eac449d1c390c2bf6dc21ff84a4c770a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:01:55 +0300

    Completed task: T08.TownInfo

---
+++ Data types and variables - Lab/01.Meters to kilometers/08.Town info/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _08.Town_info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var town = Console.ReadLine();
+            var population = uint.Parse(Console.ReadLine());
+            var area = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {town} has population of {population} and area {area} square km.");
+        }
+    }
+}
