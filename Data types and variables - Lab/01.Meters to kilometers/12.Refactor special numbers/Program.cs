4613a1acd662c1cb61413389d9bcb7977001f7f2
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-28 12:03:16 +0300

    Completed task: T12.RefactorSpecialNumbers

---
+++ Data types and variables - Lab/01.Meters to kilometers/12.Refactor special numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _12.Refactor_special_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nums = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= nums; i++)
+            {
+                var currentNumber = i;
+                var sum = 0;
+                while (currentNumber > 0)
+                {
+                    sum += currentNumber % 10;
+                    currentNumber /= 10;
+                }
+                var isSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
