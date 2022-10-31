dc4a5e26c87e3b4b401bde5650c8928faaa0201d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-31 10:53:42 +0200

    Completed task: T04.SumOfChars

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T04.SumOfChars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T04.SumOfChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars = int.Parse(Console.ReadLine());
+            var sum = 0;
+            for (int i = 1; i <= chars; i++)
+            {
+                var symbol = char.Parse(Console.ReadLine());
+                sum += symbol;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
