44340358b7f75a6a74474786bbea6eaef413a6fe
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-31 10:53:14 +0200

    Completed task: T02.SumDigits

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T02.Sum digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T02.Sum_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            var sum = 0;
+            while (num != 0)
+            {
+                var currentDigit = num % 10;
+                num /= 10;
+                sum += currentDigit;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
