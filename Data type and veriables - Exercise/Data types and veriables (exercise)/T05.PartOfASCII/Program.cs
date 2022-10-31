868b06f5889a60b37c490347012408a124dfc133
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-31 10:54:15 +0200

    Completed task: T05.PartOfASCII

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T05.PartOfASCII/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace T05.PartOfASCII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var start = int.Parse(Console.ReadLine());
+            var end = int.Parse(Console.ReadLine());
+            for (int i = start; i <= end; i++)
+            {
+                var currChar = (char)i;
+                Console.Write($"{currChar} ");
+            }
+        }
+    }
+}
