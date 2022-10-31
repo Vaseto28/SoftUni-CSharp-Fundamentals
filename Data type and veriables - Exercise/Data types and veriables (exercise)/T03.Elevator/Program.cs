cf90a90ef9ea047667054210c856e254aa841a6f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-31 10:53:25 +0200

    Completed task: T03.Elevator

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T03.Elevator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace T03.Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = int.Parse(Console.ReadLine());
+            var capacity = int.Parse(Console.ReadLine());
+            var courses = 0;
+            for (int i = people; i > 0; i-=capacity)
+            {
+                courses++;
+            }
+            Console.WriteLine(courses);
+        }
+    }
+}
