d7eba33cbbee6bf38c8bd88c7a64ebd1516d0984
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-10-31 10:53:00 +0200

    Completed task: T01.IntegerOperations

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/01.Integer operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.Integer_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            var thirdNum = int.Parse(Console.ReadLine());
+            var fourthNum = int.Parse(Console.ReadLine());
+
+            long sum = (long)firstNum + secondNum;
+            long quotient = sum / thirdNum;
+            long multiplicationProduct = quotient * fourthNum;
+
+            Console.WriteLine(multiplicationProduct);
+        }
+    }
+}
