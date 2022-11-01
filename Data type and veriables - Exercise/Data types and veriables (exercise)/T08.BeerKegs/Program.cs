a53defb33948ac49808fc4d3e53aa05a5824e6d9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-01 19:44:48 +0200

    Completed task: T09.BeerKegs

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T08.BeerKegs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T08.BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kegs = int.Parse(Console.ReadLine());
+            var biggestValue = 0.0;
+            var theBiggestKeg = String.Empty;
+            for (int i = 0; i < kegs; i++)
+            {
+                var model = Console.ReadLine();
+                var radius = double.Parse(Console.ReadLine());
+                var height = int.Parse(Console.ReadLine());
+                var currValue = Math.PI * radius * radius * height;
+                if (currValue > biggestValue)
+                {
+                    biggestValue = currValue;
+                    theBiggestKeg = model;
+                }
+            }
+            Console.WriteLine(theBiggestKeg);
+        }
+    }
+}
