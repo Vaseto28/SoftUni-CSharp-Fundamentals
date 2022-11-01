8f0e0a49139bffb7064c7069a9f81e49b8816a77
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-01 19:45:03 +0200

    Completed task: T09.SpiceFlow

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T09.SpiceFlow/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace T09.SpiceFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var startingYeld = int.Parse(Console.ReadLine());
+            long extractedSpice = 0;
+            var days = 0;
+            for (int i = startingYeld; i >= 100; i -= 10)
+            {
+                if (i<=0)
+                {
+                    extractedSpice += i;
+                }
+                else
+                {
+                    extractedSpice = extractedSpice + (i - 26);
+                }
+                days++;
+            }
+            Console.WriteLine(days);
+            if (extractedSpice < 0)
+            {
+                Console.WriteLine(extractedSpice);
+            }
+            else
+            {
+                Console.WriteLine(extractedSpice - 26);
+            }
+        }
+    }
+}
