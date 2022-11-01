d7ce5431e6d9740276fc1b2ae8c9c7547ce15328
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-01 19:45:18 +0200

    Completed task: T10.Pokemon

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/Т10.Pokemon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Т10.Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var m = int.Parse(Console.ReadLine());
+            var y = int.Parse(Console.ReadLine());
+            var counter = 0;
+            var startingN = n;
+            while (n >= m)
+            {
+                n -= m;
+                counter++;
+                if ((decimal)(0.5 * startingN) == n)
+                {
+                    if (n / y != 0)
+                    {
+                        n /= y;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(counter);
+        }
+    }
+}
