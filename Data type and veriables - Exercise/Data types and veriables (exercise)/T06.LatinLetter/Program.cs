f7bae7cff0349539014a2b84f2d30971bee044ec
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-01 19:43:35 +0200

    Completed task: T06.LatinLetter

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T06.LatinLetter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T06.LatinLetter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var letters = int.Parse(Console.ReadLine());
+            for (int i = 0; i < letters; i++)
+            {
+                for (int j = 0; j < letters; j++)
+                {
+                    for (int k = 0; k < letters; k++)
+                    {
+                        var firstChar = (char)('a' + i);
+                        var secondChar = (char)('a' + j);
+                        var thirdChar = (char)('a' + k);
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
