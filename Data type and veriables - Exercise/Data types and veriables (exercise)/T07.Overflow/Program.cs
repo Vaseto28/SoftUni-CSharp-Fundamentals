ec64f3ed438ad3a90703924e70a7f9862419b8f7
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-01 19:44:29 +0200

    Completed task: T07.Overflow

---
+++ Data type and veriables - Exercise/Data types and veriables (exercise)/T07.Overflow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T07.Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var isOverflowed = false;
+            for (int i = 1; i <= lines; i++)
+            {
+                var litres = int.Parse(Console.ReadLine());
+                sum += litres;
+                if (i>lines)
+                {
+                    if (isOverflowed)
+                    {
+                        break;
+                    }
+                }
+                if (sum > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sum -= litres;
+                    isOverflowed = true;
+                    continue;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
