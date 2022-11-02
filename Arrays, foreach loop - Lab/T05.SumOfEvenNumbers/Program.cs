1730cc95f29eaaba42480d8e40df7aaa11cbba8e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-02 10:21:22 +0200

    Completed task: T05.SumOfEvenNumbers

---
+++ Arrays, foreach loop - Lab/T05.SumOfEvenNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T05.SumOfEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = Console.ReadLine().Split();
+            var sum = 0;
+            foreach (var item in nums)
+            {
+                if (int.Parse(item) % 2 == 0)
+                {
+                    sum += int.Parse(item);
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
