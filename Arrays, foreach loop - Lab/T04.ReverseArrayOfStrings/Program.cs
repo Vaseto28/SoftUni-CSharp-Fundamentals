6f1ef5f60d16314ec5f7846d8b10881c5c78429b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-02 10:21:00 +0200

    Completed task: T04.ReverseArrayOfStrings

---
+++ Arrays, foreach loop - Lab/T04.ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace T04.ReverseArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var items = Console.ReadLine().Split();
+            Array.Reverse(items);
+            Console.WriteLine(String.Join(" ", items));
+        }
+    }
+}
