ac7eb5a882fb0d3ca781a4535854abd5fcb58b56
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-02 10:20:26 +0200

    Completed task: T03.RoundingNumbers

---
+++ Arrays, foreach loop - Lab/T03.RoundingNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace T03.RoundingNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] nums = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var currItem = Math.Round(nums[i], MidpointRounding.AwayFromZero);
+                Console.WriteLine($"{nums[i]} => {currItem}");
+            }
+        }
+    }
+}
