58bf5fc5d75569d00f05a45226c4d9f9f0ade0ec
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-02 10:20:10 +0200

    Completed task: T02.ReverseNumbers

---
+++ Arrays, foreach loop - Lab/T02.ReverseNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T02.ReverseNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = int.Parse(Console.ReadLine());
+            int[] numbers = new int[nums];
+            for (int i = 0; i < nums; i++)
+            {
+                var currNum = int.Parse(Console.ReadLine());
+                numbers[i] = currNum;
+            }
+            for (int i = numbers.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
+        }
+    }
+}
