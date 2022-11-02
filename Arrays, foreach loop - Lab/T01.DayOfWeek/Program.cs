67c74562ef6741261fefa8c845ceba22783ce1d8
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-02 10:19:58 +0200

    Completed task: T01.DayOfWeek

---
+++ Arrays, foreach loop - Lab/T01.DayOfWeek/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var day = int.Parse(Console.ReadLine());
+            string[] dayOfWeek = {
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday"};
+            if (day > 0 && day <= dayOfWeek.Length)
+            {
+                Console.WriteLine(dayOfWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
