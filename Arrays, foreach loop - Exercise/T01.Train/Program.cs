5cd0c3f98f9ea68d223ed79096cd2c29f0ca981e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-03 10:02:04 +0200

    Completed task: T01.Train

---
+++ Arrays, foreach loop - Exercise/T01.Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace T01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wagoons = int.Parse(Console.ReadLine());
+            int[] peopleInWagoons = new int[wagoons];
+            var sum = 0;
+            for (int i = 0; i < wagoons; i++)
+            {
+                var peopleInCurrWagoon = int.Parse(Console.ReadLine());
+                peopleInWagoons[i] = peopleInCurrWagoon;
+                sum += peopleInCurrWagoon;
+            }
+            Console.WriteLine(String.Join(" ", peopleInWagoons));
+            Console.WriteLine(sum);
+        }
+    }
+}
