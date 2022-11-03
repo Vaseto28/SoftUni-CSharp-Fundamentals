a6f70c9c7e5d8b810752e29095a0985fc9b793fd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-03 10:00:55 +0200

    Completed task: T08.condenseArrayToNumber

---
+++ Arrays, foreach loop - Lab/T08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace T08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var firstLength = nums.Length;
+            for (int i = 0; i < firstLength - 1; i++)
+            {
+                int[] condensed = new int[nums.Length];
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    condensed[j] = nums[j] + nums[j + 1];
+                }
+                nums = condensed;
+            }
+            Console.WriteLine(nums[0]);
+        }
+    }
+}
