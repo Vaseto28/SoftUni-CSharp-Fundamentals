1d2b47017dd4ecb656423f03def67c0a69f9d0d1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-03 10:00:10 +0200

    Completed task: T06.EvenOrOddSubtraction

---
+++ Arrays, foreach loop - Lab/T06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T06.EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = Console.ReadLine().Split();
+            var evenSum = 0;
+            var oddSum = 0;
+            var diff = 0;
+            foreach (var item in nums)
+            {
+                if (int.Parse(item) % 2 == 0)
+                {
+                    evenSum += int.Parse(item);
+                }
+                else
+                {
+                    oddSum += int.Parse(item);
+                }
+            }
+            diff = evenSum - oddSum;
+            Console.WriteLine(diff);
+        }
+    }
+}
