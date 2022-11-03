9e37d0b214433774bd123904a5451d6a37a9b3c1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-03 10:02:17 +0200

    Completed task: T02.CommonElements

---
+++ Arrays, foreach loop - Exercise/T02.CommonElements/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace T02.CommonElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array1 = Console.ReadLine().Split();
+            var array2 = Console.ReadLine().Split();
+            foreach (var item in array2)
+            {
+                foreach (var item1 in array1)
+                {
+                    if (item == item1)
+                    {
+                        Console.Write($"{item} ");
+                    }
+                }
+            }
+        }
+    }
+}
