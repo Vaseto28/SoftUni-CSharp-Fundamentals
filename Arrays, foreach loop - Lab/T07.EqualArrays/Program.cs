f12100426529556a013103cf3e9bac9e5c6af5ee
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-03 10:00:34 +0200

    Completed task: T07.EqualArrays

---
+++ Arrays, foreach loop - Lab/T07.EqualArrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace T07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstArray = Console.ReadLine().Split();
+            var secondArray = Console.ReadLine().Split();
+            var isIdentical = true;
+            var sum = 0;
+            for (int i = 0; i < firstArray.Length; i++)
+            {
+                if (firstArray[i] != secondArray[i])
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    isIdentical = false;
+                    break;
+                }
+            }
+            if (isIdentical)
+            {
+                for (int j = 0; j < firstArray.Length; j++)
+                {
+                    sum += int.Parse(firstArray[j]);
+                }
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
