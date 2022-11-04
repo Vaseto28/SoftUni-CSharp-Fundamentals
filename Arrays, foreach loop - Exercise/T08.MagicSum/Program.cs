85290bd6c6fabe31fb1eed39ee79ca70d6b65a23
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-04 11:51:13 +0200

    Completed task: T08.MagicSum

---
+++ Arrays, foreach loop - Exercise/T08.MagicSum/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace T08.MagicSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var magicSum = int.Parse(Console.ReadLine());
+            for (int i = 0; i < array.Length; i++)
+            {
+                var firstNum = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    var secondNum = array[j];
+                    if (firstNum + secondNum == magicSum)
+                    {
+                        Console.Write($"{firstNum} {secondNum}");
+                        Console.WriteLine();
+                    }
+                }
+            }
+        }
+    }
+}
