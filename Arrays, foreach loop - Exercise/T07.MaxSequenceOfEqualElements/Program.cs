bad0cfdee1b1e2053f00651a9224d3fcb4a06011
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-04 11:50:57 +0200

    Completed task: T07.MaxSequenceOfEqualElements

---
+++ Arrays, foreach loop - Exercise/T07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace T07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var sequenceCounter = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                var currNum = array[i];
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    var nextNum = array[j];
+                    if (currNum != nextNum)
+                    {
+                        sequenceCounter = 0;
+                        break;
+                    }
+                    else
+                    {
+                        sequenceCounter++;
+                        Console.Write($"{currNum} ");
+                    }
+                }
+            }
+        }
+    }
+}
