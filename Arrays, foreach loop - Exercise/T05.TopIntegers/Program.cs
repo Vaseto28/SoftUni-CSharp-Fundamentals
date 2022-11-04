49c9c68a21a0915177cfc44d8e72bc9c5b64ca42
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-04 11:50:20 +0200

    Completed task: T05.TopIntegers

---
+++ Arrays, foreach loop - Exercise/T05.TopIntegers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.TopIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            for (int i = 0; i < array.Length; i++)
+            {
+                var currElement = array[i];
+                var isTopInteger = true;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    var nextElement = array[j];
+                    if (nextElement >= currElement)
+                    {
+                        isTopInteger = false;
+                        break;
+                    }
+                }
+                if (isTopInteger)
+                {
+                    Console.Write($"{currElement} ");
+                }
+            }
+        }
+    }
+}
