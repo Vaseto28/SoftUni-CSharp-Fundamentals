8aa856ba25bee3350bf7b7dd54bf8ef77b7f60a3
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-04 11:49:58 +0200

    Completed task: T04.ArrayRotation

---
+++ Arrays, foreach loop - Exercise/T04.ArrayRotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace T04.ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var rotations = int.Parse(Console.ReadLine());
+            for (int currRotation = 1; currRotation <= rotations; currRotation++)
+            {
+                int firstElement = array[0];
+                for (int i = 1; i < array.Length; i++)
+                {
+                    array[i - 1] = array[i];
+                }
+                array[array.Length - 1] = firstElement;
+            }
+            Console.WriteLine(String.Join(" ", array));
+        }
+    }
+}
