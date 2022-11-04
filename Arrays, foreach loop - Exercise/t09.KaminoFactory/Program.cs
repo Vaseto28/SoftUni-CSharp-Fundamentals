eadf5b1bb594a280d360ef252c468a104164b8cd
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-04 11:51:30 +0200

    Completed task: T09.KaminoFactory

---
+++ Arrays, foreach loop - Exercise/t09.KaminoFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace t09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
