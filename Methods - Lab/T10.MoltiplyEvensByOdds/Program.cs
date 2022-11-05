1d20b3ef93d51d7c2058684ea13d61e23522bb2c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-06 01:02:29 +0200

    Completed task: T10.MultiplyEvensByOdds

---
+++ Methods - Lab/T10.MoltiplyEvensByOdds/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T10.MoltiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = Math.Abs(int.Parse(Console.ReadLine()));
+            var evenSum = GetTheEvenSum(num);
+            var oddSum = GetTheOddSum(num);
+            var multiplication = evenSum * oddSum;
+            Console.WriteLine(multiplication);
+        }
+
+        static int GetTheEvenSum(int num)
+        {
+            var evenSum = 0;
+            while (num!=0)
+            {
+                var currNum = num % 10;
+                if (currNum%2==0)
+                {
+                    evenSum += currNum;
+                }
+                num /= 10;
+            }
+            return evenSum;
+        }
+
+        static int GetTheOddSum(int num)
+        {
+            var oddSum = 0;
+            while (num != 0)
+            {
+                var currNum = num % 10;
+                if (currNum % 2 != 0)
+                {
+                    oddSum += currNum;
+                }
+                num /= 10;
+            }
+            return oddSum;
+        }
+    }
+}
