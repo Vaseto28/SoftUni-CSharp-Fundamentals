7e2b3a689dcaba2b77093c38260a10ffc2f02ecf
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-05 13:38:50 +0200

    Completed task: T04.PrintingTriangle

---
+++ Methods - Lab/T04.PrintingTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace T04.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            for (int i = 1; i < num; i++)
+            {
+                PrintingOneLine(i);
+            }
+            for (int i = num; i >= 1; i--)
+            {
+                PrintingOneLine(i);
+            }
+        }
+
+        static void PrintingOneLine(int num)
+        {
+            for (int i = 1; i <= num; i++)
+            {
+                Console.Write($"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
