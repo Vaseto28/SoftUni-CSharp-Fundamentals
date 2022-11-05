f1c25b999659977ac3a6ff94f9bd79fa6e632126
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-06 01:00:29 +0200

    Completed task: T06.CalculatingRectangleArea

---
+++ Methods - Lab/T06.CalculatingRectangleArea/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T06.CalculatingRectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var width = int.Parse(Console.ReadLine());
+            var length = int.Parse(Console.ReadLine());
+            var area = CalculatingRectangleArea(width, length);
+            Console.WriteLine(area);
+        }
+
+        static int CalculatingRectangleArea(int width, int length)
+        {
+            var area = width * length;
+            return area;
+        }
+
+        //Alternative way
+        //static void Main(string[] args)
+        //{
+        //    var width = int.Parse(Console.ReadLine());
+        //    var length = int.Parse(Console.ReadLine());
+        //    CalculatingRectangleArea(width, length);
+        //}
+
+        //static void CalculatingRectangleArea(int width, int length)
+       //{
+        //    var sum = width * length;
+        //    Console.WriteLine(sum);
+        //}
+
+    }
+}
