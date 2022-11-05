1a062a58b9b2dbfad5ebedada9d49a031b0c86f1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-05 13:38:33 +0200

    Completed task: T03.Calculations

---
+++ Methods - Lab/T03.Calculations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace T03.Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var action = Console.ReadLine();
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            CalculatingNumbers(action, firstNum, secondNum);
+        }
+
+        static void CalculatingNumbers(string action, int firstNum, int secondNum)
+        {
+            switch (action)
+            {
+                case "add":
+                    var sum = firstNum + secondNum;
+                    Console.WriteLine(sum);
+                    break;
+                case "subtract":
+                    var subtraction = firstNum - secondNum;
+                    Console.WriteLine(subtraction);
+                    break;
+                case "multiply":
+                    var multiplication = firstNum * secondNum;
+                    Console.WriteLine(multiplication);
+                    break;
+                case "devide":
+                    var devision = firstNum / secondNum;
+                    Console.WriteLine(devision);
+                    break;
+            }
+        }
+    }
+}
