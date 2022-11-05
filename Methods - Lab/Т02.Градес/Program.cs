bf5744731d6923890415e6297be912c7a0414d6f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-05 13:38:19 +0200

    Completed task: T02.Grades

---
+++ Methods - Lab/Т02.Градес/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Т02.Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grade = double.Parse(Console.ReadLine());
+            CalculatingGrade(grade);
+        }
+
+        static void CalculatingGrade(double grade)
+        {
+            if (grade >= 2.00 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade >= 3 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade >= 3.50 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade >= 4.50 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
