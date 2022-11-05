492299f704a31622e743ac33491f8840ceb5b137
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-06 01:01:47 +0200

    Completed task: T09.GraterOfTwoValues

---
+++ Methods - Lab/T09.GraterOfTwoValues/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace T09.GraterOfTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var type = Console.ReadLine();
+            switch (type)
+            {
+                case "int":
+                    var firstIntValue = int.Parse(Console.ReadLine());
+                    var secondIntValue = int.Parse(Console.ReadLine());
+                    var graterIntValue = GraterIntValue(firstIntValue, secondIntValue);
+                    Console.WriteLine(graterIntValue);
+                    break;
+                case "char":
+                    var firstCharValue = char.Parse(Console.ReadLine());
+                    var secondCharValue = char.Parse(Console.ReadLine());
+                    var graterCharValue = GraterCharValue(firstCharValue, secondCharValue);
+                    Console.WriteLine(graterCharValue);
+                    break;
+                case "string":
+                    var firstStringValue = Console.ReadLine();
+                    var secondStringValue = Console.ReadLine();
+                    var graterStringValue = GraterStringValue(firstStringValue, secondStringValue);
+                    Console.WriteLine(graterStringValue);
+                    break;
+            }
+        }
+
+        static int GraterIntValue(int firstValue, int secondValue)
+        {
+            return (firstValue > secondValue) ? firstValue : secondValue;
+        }
+        static char GraterCharValue(char firstValue, char secondValue)
+        {
+            return (firstValue > secondValue) ? firstValue : secondValue;
+        }
+        static string GraterStringValue(string firstValue, string secondValue)
+        {
+            return firstValue.CompareTo(secondValue) >= 0 ? firstValue : secondValue;
+        }
+    }
+}
