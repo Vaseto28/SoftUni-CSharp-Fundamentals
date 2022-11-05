0af241fbf8219ed54808d87327cb1dd634c1bfee
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-06 01:01:13 +0200

    Completed task: T08.MathPower

---
+++ Methods - Lab/T08.MathPower/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace T08.MathPower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = double.Parse(Console.ReadLine());
+            var power = int.Parse(Console.ReadLine());
+            var poweredNum = PoweringTheNumber(num, power);
+            Console.WriteLine(poweredNum);
+        }
+
+        static double PoweringTheNumber(double num, int power)
+        {
+            var poweredNum = 1.0;
+            for (int i = 1; i <= power; i++)
+            {
+                poweredNum *= num;
+            }
+            return poweredNum;
+        }
+    }
+}
