dc5ed1772a5063606458494365c882f4c828770a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-07 09:19:35 +0200

    Completed task: T01.SmallestOfThreeNumbers

---
+++ Methods - Exercise/T01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace T01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num1 = int.Parse(Console.ReadLine());
+            var num2 = int.Parse(Console.ReadLine());
+            var num3 = int.Parse(Console.ReadLine());
+            PrintingTheSmallestNumber(num1, num2, num3);
+        }
+
+        static void PrintingTheSmallestNumber(int num1, int num2, int num3)
+        {
+            if (num1 < num2 && num1 < num3)
+            {
+                Console.WriteLine(num1);
+            }
+            else if (num2 < num1 && num2 < num3)
+            {
+                Console.WriteLine(num2);
+            }
+            else
+            {
+                Console.WriteLine(num3);
+            }
+        }
+    }
+}
