ff2f444a584fd460d405be28664e47844e47ca7e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-07 09:21:30 +0200

    Completed task: T02.VowelsCount

---
+++ Methods - Exercise/T02.VowelsCount/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace T02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine();
+            FindingTheVowelLetters(text);
+        }
+
+        static void FindingTheVowelLetters(string text)
+        {
+            var vowelCounter = 0;
+            for (int i = 0; i < text.Length; i++)
+            {
+                var currLetter = text[i];
+                if (currLetter == 'a' || currLetter == 'e' || currLetter == 'i' || currLetter == 'o' || currLetter == 'u' || currLetter == 'A' || currLetter == 'E' || currLetter == 'I' || currLetter == 'O' || currLetter == 'U')
+                {
+                    vowelCounter++;
+                }
+            }
+            Console.WriteLine(vowelCounter);
+        }
+    }
+}
