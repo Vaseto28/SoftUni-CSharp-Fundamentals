6fdb97f353e549340994b51ba364c428f233e7f0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-07 09:21:47 +0200

    Completed task: T03.CharactersInRange

---
+++ Methods - Exercise/T03.CharactersInRange/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace T03.CharactersInRange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstChar = char.Parse(Console.ReadLine());
+            var secondChar = char.Parse(Console.ReadLine());
+            CharactersInInterval(firstChar, secondChar);
+        }
+
+        static void CharactersInInterval(char firstChar, char secondChar)
+        {
+            StringBuilder result = new StringBuilder();
+            var theRightValueOfTheFirstChar = firstChar;
+            if (secondChar<firstChar)
+            {
+                firstChar = secondChar;
+                secondChar = theRightValueOfTheFirstChar;
+            }
+            for (char i = ++firstChar; i < secondChar; i++)
+            {
+                result.Append($"{i} ");
+            }
+            Console.Write(result);
+        }
+    }
+}
