95907e9861d1e22041684e137ddf8c7a55e19d6a
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-08 18:28:19 +0200

    Completed task: T01.SumAdjestentEqualNumbers

---
+++ Lists - Lab/T01.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine().Split().Select(decimal.Parse).ToList();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i] = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i + 1);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
