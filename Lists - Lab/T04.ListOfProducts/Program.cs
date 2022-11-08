73f4680ae45ec280878e270b1ac87bbd2b329aec
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-08 18:29:21 +0200

    Completed task: T04.ListOfProducts

---
+++ Lists - Lab/T04.ListOfProducts/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T04.ListOfProducts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productsCount = int.Parse(Console.ReadLine());
+            List<string> products = new List<string>();
+            for (int i = 0; i < productsCount; i++)
+            {
+                string currProduct = Console.ReadLine();
+                products.Add(currProduct);
+            }
+            products.Sort();
+            for (int i = 0; i < products.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}.{products[i]}");
+            }
+        }
+    }
+}
