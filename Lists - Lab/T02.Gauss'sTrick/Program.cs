ae48aabbf89d6e89f75e4806d3fe6bb5cacaacc9
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-08 18:28:39 +0200

    Completed task: T02.Gauss'sTrick

---
+++ Lists - Lab/T02.Gauss'sTrick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T02.Gauss_sTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < numbers.Count / 2; i++)
+            {
+                result.Add(numbers[i] + numbers[numbers.Count - i - 1]);
+            }
+
+            if (numbers.Count % 2 != 0)
+            {
+                result.Add(numbers[numbers.Count / 2]);
+            }
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
