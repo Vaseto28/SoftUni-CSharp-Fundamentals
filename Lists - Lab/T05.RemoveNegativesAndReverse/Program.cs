f24e9292dfb4e9580bd19d405cca0bef638ebe09
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-08 18:29:43 +0200

    Completed task: T05.RemoveNegativesAndReverse

---
+++ Lists - Lab/T05.RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T05.RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            numbers.RemoveAll(negativeNums => negativeNums < 0);
+            numbers.Reverse();
+            if (numbers.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(" ", numbers));
+            }
+        }
+    }
+}
