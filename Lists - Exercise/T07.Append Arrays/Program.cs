d0b2a5244075dffa63ba3c6479764b9ac7611c56
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-10 11:27:31 +0200

    Completed task: T07.AppendArrays

---
+++ Lists - Exercise/T07.Append Arrays/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T07.Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> output = new List<string>();
+            var input = Console.ReadLine();
+            string[] arrayOfInput = input.Split('|', StringSplitOptions.RemoveEmptyEntries).ToArray();
+            for (int i = arrayOfInput.Length - 1; i >= 0; i--)
+            {
+                string[] currArray = arrayOfInput[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+                output.AddRange(currArray);
+            }
+
+            Console.WriteLine(String.Join(" ", output));
+        }
+    }
+}
