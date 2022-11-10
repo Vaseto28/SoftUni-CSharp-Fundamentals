4235ec4ea1d7fa6ecd5ab7e98094f0fbc6ae2a31
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-10 11:26:00 +0200

    Completed task: T01.Train

---
+++ Lists - Exercise/T01.Train/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace T01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> passangers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            int capacityOfTheWagoons = int.Parse(Console.ReadLine());
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] arguments = command.Split().ToArray();
+                if (arguments.Length == 2)
+                {
+                    var newWagoonPassangers = int.Parse(arguments[1]);
+                    passangers.Add(newWagoonPassangers);
+                }
+                else if (arguments.Length == 1)
+                {
+                    var newPassangers = int.Parse(arguments[0]);
+                    for (int i = 0; i < passangers.Count; i++)
+                    {
+                        if (newPassangers + passangers[i] <= capacityOfTheWagoons)
+                        {
+                            passangers[i] += newPassangers;
+                            break;
+                        }
+                        else if (i == passangers.Count)
+                        {
+                            passangers.Add(newPassangers);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(String.Join(" ", passangers));
+        }
+    }
+}
