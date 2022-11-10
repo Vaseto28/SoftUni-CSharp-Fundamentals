d9e467f251bf6d76dec53a510d9666d963e4d2c2
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-10 11:26:14 +0200

    Completed task: T02.ChangeList

---
+++ Lists - Exercise/T02.Change List/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace T02.Change_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] arguments = command.Split().ToArray();
+                var action = arguments[0];
+                switch (action)
+                {
+                    case "Delete":
+                        var elementsToDelete = int.Parse(arguments[1]);
+                        numbers.RemoveAll(x => x == elementsToDelete);
+                        break;
+                    case "Insert":
+                        var element = int.Parse(arguments[1]);
+                        var index = int.Parse(arguments[2]);
+                        numbers.Insert(index, element);
+                        break;
+                }
+            }
+            Console.WriteLine(String.Join(" ", numbers));
+        }
+    }
+}
