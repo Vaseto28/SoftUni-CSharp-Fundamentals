8a4c8e9b682f57a5c668c86bb0221fd0d99b603e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-10 11:27:16 +0200

    Completed task: T06.CardsGames

---
+++ Lists - Exercise/T06.Cards Games/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace T06.Cards_Games
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondDeck = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            while (true)
+            {
+                var currCardFromFirstDeck = firstDeck[0];
+                var currCardFromSecondDeck = secondDeck[0];
+                if (currCardFromFirstDeck > currCardFromSecondDeck)
+                {
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                    firstDeck.Add(currCardFromSecondDeck);
+                    firstDeck.Add(currCardFromFirstDeck);
+                }
+                else if (currCardFromFirstDeck < currCardFromSecondDeck)
+                {
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                    secondDeck.Add(currCardFromFirstDeck);
+                    secondDeck.Add(currCardFromSecondDeck);
+                }
+                else
+                {
+                    firstDeck.RemoveAt(0);
+                    secondDeck.RemoveAt(0);
+                }
+
+                if (firstDeck.Count == 0 || secondDeck.Count == 0)
+                {
+                    if (firstDeck.Count == 0)
+                    {
+                        Console.WriteLine($"Second player wins! Sum: {secondDeck.Sum()}");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"First player wins! Sum: {firstDeck.Sum()}");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
