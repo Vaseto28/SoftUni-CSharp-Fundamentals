c9422e257fa87e6c55d2e18341316803ebcb8f05
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-11 09:11:22 +0200

    Completed task: T02.BigFactorial

---
+++ Objects and classes - Lab/T02.BigFactorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Numerics;
+
+namespace T02.BigFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var num = int.Parse(Console.ReadLine());
+            BigInteger factorialOfTheNumber = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                factorialOfTheNumber *= i;
+            }
+            Console.WriteLine(factorialOfTheNumber);
+        }
+    }
+}
