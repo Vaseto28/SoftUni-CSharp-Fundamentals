3de8bf0fed2c62cd1a412408c70aaacd2b71557e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-11 09:11:07 +0200

    Completed task: T01.RandomizeWords

---
+++ Objects and classes - Lab/T01.RandomizeWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T01.RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            Random randomizedIndex = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                var currRandomIndex = randomizedIndex.Next(0, words.Length);
+                var currElement = words[i];
+                words[i] = words[currRandomIndex];
+                words[currRandomIndex] = currElement; 
+            }
+
+            Console.WriteLine(String.Join(Environment.NewLine, words));
+        }
+    }
+}
