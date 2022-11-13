ee8e7007b250f30e9c991419242960f45f40c0e5
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-13 15:45:52 +0200

    Completed task: T01.CountCharsInString

---
+++ Associative arrays - Lab/T01.CountCharsInString/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T01.CountCharsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<char, int> chars = new Dictionary<char, int>();
+
+            foreach (string word in words)
+            {
+                char[] currWord = word.ToCharArray();
+
+                for (int i = 0; i < currWord.Length; i++)
+                {
+                    if (!chars.ContainsKey(currWord[i]))
+                    {
+                        chars.Add(currWord[i], 0);
+                    }
+
+                    chars[currWord[i]]++;
+                }
+            }
+
+            foreach (var kvp in chars)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
