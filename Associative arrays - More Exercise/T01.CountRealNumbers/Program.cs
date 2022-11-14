1748ecbe878dd7564dff8516bb7b6e18151d8680
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-14 09:57:16 +0200

    Completed task: T01.CountRealNumbers

---
+++ Associative arrays - More Exercise/T01.CountRealNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T01.CountRealNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<int, int> frequences = new SortedDictionary<int, int>();
+
+            int[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(num => int.Parse(num))
+                .ToArray();
+
+            foreach (int number in numbers)
+            {
+                if (frequences.ContainsKey(number))
+                {
+                    frequences[number]++;
+                }
+                else
+                {
+                    frequences.Add(number, 1);
+                }
+            }
+
+            foreach (var kvp in frequences)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
