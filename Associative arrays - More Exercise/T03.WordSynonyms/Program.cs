bf77d6bbe376725ecaf94e94dff897f6f33af383
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-14 09:58:07 +0200

    Completed task: T03.WordSynonyms

---
+++ Associative arrays - More Exercise/T03.WordSynonyms/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T03.WordSynonyms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int wordSynonymPairs = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> wordsSynonyms = new Dictionary<string, List<string>>();
+
+            for (int i = 1; i <= wordSynonymPairs; i++)
+            {
+                string word = Console.ReadLine();
+                string synonym = Console.ReadLine();
+
+                if (!wordsSynonyms.ContainsKey(word))
+                {
+                    wordsSynonyms.Add(word, new List<string>());
+                }
+                
+                wordsSynonyms[word].Add(synonym);
+            }
+
+            foreach (var word in wordsSynonyms)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
+            }
+        }
+    }
+}
