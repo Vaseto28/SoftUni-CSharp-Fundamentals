88a699c981ae4fa10556192085895d179621e06e
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-14 09:58:25 +0200

    Completed task: T04.WordFilter

---
+++ Associative arrays - More Exercise/T04.WordFilter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace T04.WordFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] filteredWords = words.Where(word => word.Length % 2 == 0).ToArray();
+
+            foreach (string word in filteredWords)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
