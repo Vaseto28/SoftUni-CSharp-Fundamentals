d09ec3f9366f0209e45cd5c38fee1342db2812b8
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-14 09:57:52 +0200

    Completed task: T02.OddOccurrences

---
+++ Associative arrays - More Exercise/T02.OddOccurrences/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02.OddOccurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, int> occurrencies = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                string currWord = word.ToLower();
+
+                if (!occurrencies.ContainsKey(currWord))
+                {
+                    occurrencies.Add(currWord, 0);
+                }
+
+                occurrencies[currWord]++;
+            }
+
+            foreach (var kvp in occurrencies)
+            {
+                if (kvp.Value % 2 != 0)
+                {
+                    Console.Write($"{kvp.Key} ");
+                }
+            }
+        }
+    }
+}
