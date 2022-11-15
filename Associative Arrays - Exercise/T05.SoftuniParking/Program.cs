78cdc428bf98e43f61eeec330c164bd30f79bd58
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-15 09:59:05 +0200

    Completed task: T05.SoftuniParking

---
+++ Associative Arrays - Exercise/T05.SoftuniParking/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T05.SoftuniParking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> usersNumbersPairs = new Dictionary<string, string>();
+
+            int users = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= users; i++)
+            {
+                string command = Console.ReadLine();
+                string[] info = command.Split(' ');
+                string action = info[0];
+                if (action == "register")
+                {
+                    string username = info[1];
+                    string licensePlateNumber = info[2];
+
+                    if (usersNumbersPairs.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                        continue;
+                    }
+
+                    usersNumbersPairs.Add(username, licensePlateNumber);
+                    Console.WriteLine($"{username} registered {licensePlateNumber} successfully");
+                    continue;
+                }
+                else if (action == "unregister")
+                {
+                    string username = info[1];
+                    if (!usersNumbersPairs.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                        continue;
+                    }
+
+                    usersNumbersPairs.Remove(username);
+                    Console.WriteLine($"{username} unregistered successfully");
+                    continue;
+                }
+            }
+
+            foreach (var user in usersNumbersPairs)
+            {
+                Console.WriteLine($"{user.Key} => {user.Value}");
+            }
+        }
+    }
+}
