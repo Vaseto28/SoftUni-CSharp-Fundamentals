0727463a2a13d3458c692e181c3c1a812b303e7d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-15 09:57:59 +0200

    Completed task: T02.MinerTask

---
+++ Associative Arrays - Exercise/T02.MinerTask/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T02.AMinerTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource;
+            Dictionary<string, int> resources = new Dictionary<string, int>();
+
+            while ((resource = Console.ReadLine()) != "stop")
+            {
+                int quantity = int.Parse(Console.ReadLine());
+
+                if (!resources.ContainsKey(resource))
+                {
+                    resources.Add(resource, 0);
+                }
+
+                resources[resource] += quantity;
+            }
+
+            foreach (var kvp in resources)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
