84c44cadd9a31b8f595d6d87ba44218cd5ae71ae
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-15 09:59:48 +0200

    Completed task: T08.CompanyUsers

---
+++ Associative Arrays - Exercise/T08.CompanyUsers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace T08.CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "End")
+            {
+                string[] info = command.Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = info[0];
+                string companyEployeeId = info[1];
+
+                if (companies.ContainsKey(companyName))
+                {
+                    if (!companies[companyName].Contains(companyEployeeId))
+                    {
+                        companies[companyName].Add(companyEployeeId);
+                        continue;
+                    }
+
+                    continue;
+                }
+
+                companies.Add(companyName, new List<string> { companyEployeeId });
+            }
+
+            foreach (var company in companies)
+            {
+                Console.WriteLine(company.Key);
+                foreach (var companyEmployeeId in company.Value)
+                {
+                    Console.WriteLine($"-- {companyEmployeeId}");
+                }
+            }
+        }
+    }
+}
