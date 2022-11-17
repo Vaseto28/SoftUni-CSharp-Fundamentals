65e9c10795dd42942529903e1b918debd2e00999
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-17 11:08:00 +0200

    Completed task: T03.Substring

---
+++ Text processing - Lab/T03.Substring/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringToSubtract = Console.ReadLine();
+            string @string = Console.ReadLine();
+
+            while (@string.Contains(stringToSubtract))
+            {
+                @string = @string.Replace(stringToSubtract, string.Empty);
+            }
+
+            Console.WriteLine(@string);
+        }
+    }
+}
