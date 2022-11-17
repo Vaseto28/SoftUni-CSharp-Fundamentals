9f7c0c6c1f2969d5e7362acab7de85e5860ead02
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-17 11:09:24 +0200

    Completed task: T05.DigitsLettersAndOther

---
+++ Text processing - Lab/T05.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace T05.DigitsLettersAndOther
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            char[] chars = text.ToCharArray();
+
+            char[] digits = text.Where(character => char.IsDigit(character)).ToArray();
+            char[] letters = text.Where(character => char.IsLetter(character)).ToArray();
+            char[] other = text.Where(character => !char.IsLetterOrDigit(character)).ToArray();
+
+            Console.WriteLine(String.Join("", digits));
+            Console.WriteLine(String.Join("", letters));
+            Console.WriteLine(String.Join("", other));
+        }
+    }
+}
