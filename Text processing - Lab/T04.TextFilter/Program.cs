e9e383c270eaca674aa8dfb1e280d1d4ef3985ca
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-17 11:08:13 +0200

    Completed task: T04.TextFilter

---
+++ Text processing - Lab/T04.TextFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace T04.TextFilter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] banList = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            foreach (var word in banList)
+            {
+                text = text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
