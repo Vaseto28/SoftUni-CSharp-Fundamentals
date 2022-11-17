9bf7657cf3bc6a584a82b37e21fd13f02b02c5a1
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-17 11:07:45 +0200

    Completed task: T02.RepeatStrings

---
+++ Text processing - Lab/T02.RepeatStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace T02.RepeatStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var word in words)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write(word);
+                }
+            }
+        }
+    }
+}
