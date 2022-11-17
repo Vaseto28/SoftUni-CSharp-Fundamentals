4541e1277f516130a93eaedf92f7593584cdde85
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-17 11:07:28 +0200

    Completed task: T01.ReverseStrings

---
+++ Text processing - Lab/T01.ReverseStrings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace T01.ReverseStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                char[] currWord = word.ToCharArray();
+                Array.Reverse(currWord);
+                string reversedWord = new string(currWord);
+
+                Console.WriteLine($"{word} = {reversedWord}");
+            }
+        }
+    }
+}
