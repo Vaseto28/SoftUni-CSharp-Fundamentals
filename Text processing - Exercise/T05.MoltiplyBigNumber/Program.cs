096380216bd096c98358372c866c8bf202a2f14c
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-19 16:06:08 +0200

    Completed task: T05.MultiplyBigNumber

---
+++ Text processing - Exercise/T05.MoltiplyBigNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace T05.MoltiplyBigNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger firstNum = BigInteger.Parse(Console.ReadLine());
+            int nextNum = int.Parse(Console.ReadLine());
+
+            BigInteger sum = new BigInteger();
+
+            sum = firstNum * nextNum;
+
+            Console.WriteLine(sum);
+        }
+    }
+}
