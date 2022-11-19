139d9165e408c531073d3917b0c288485209719d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-19 16:05:35 +0200

    Completed task: T04.CesarCypher

---
+++ Text processing - Exercise/T04.CesarCipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace T04.CesarCipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder encryptedText = new StringBuilder();
+
+            foreach (var character in text)
+            {
+                int encryptedCharacterInInt = character + 3;
+                char encryptedCharacter = (char)encryptedCharacterInInt;
+                encryptedText.Append(encryptedCharacter);
+            }
+
+            Console.WriteLine(encryptedText.ToString());
+        }
+    }
+}
