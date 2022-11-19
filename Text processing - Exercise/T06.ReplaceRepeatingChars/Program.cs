af7b4c29e181c193c7e9aaf3c4fbde2e7494e8e7
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-19 16:06:38 +0200

    Completed task: T06.ReplaceRepeatingChars

---
+++ Text processing - Exercise/T06.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text;
+
+namespace T06.ReplaceRepeatingChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string result = String.Empty;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currCharacter = text[i];
+                if (i == text.Length - 1)
+                {
+                    if (result.Contains(currCharacter))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        result += currCharacter;
+                        break;
+                    }
+                }
+                else
+                {
+                    char nextCharacter = text[i + 1];
+                    if (!(currCharacter == nextCharacter))
+                    {
+                        if (result.Contains(currCharacter))
+                        {
+                            result += nextCharacter;
+                            continue;
+                        }
+
+                        if (result.Contains(nextCharacter))
+                        {
+                            continue;
+                        }
+
+                        result += currCharacter;
+                        result += nextCharacter;
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
