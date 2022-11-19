e3c6b30f8921c7b73302e69fff4b169464b4de2b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-19 16:05:07 +0200

    Completed task: T03.ExtractFile

---
+++ Text processing - Exercise/T03.ExtractFile/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T03.ExtractFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = Console.ReadLine();
+            string fileName = string.Empty;
+            string fileExtension = string.Empty;
+
+            string[] fileProperties = filePath.Split('\\', StringSplitOptions.RemoveEmptyEntries);
+
+            string fileNameAndExtensions = fileProperties[fileProperties.Length - 1];
+
+            string[] filePropertiesAdvanced = fileNameAndExtensions.Split('.', StringSplitOptions.RemoveEmptyEntries);
+
+            fileName = filePropertiesAdvanced[0];
+            fileExtension = filePropertiesAdvanced[1];
+
+            Console.WriteLine($"File name: {fileName}");
+            Console.WriteLine($"File extension: {fileExtension}");
+        }
+    }
+}
