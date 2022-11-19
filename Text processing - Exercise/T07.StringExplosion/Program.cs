2ece47d7c25ad6fa293217da7b1d17642ba5e45b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-19 16:06:56 +0200

    Completed task: T07.StringExplosion

---
+++ Text processing - Exercise/T07.StringExplosion/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+
+namespace T07.StringExplosion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string bombField = Console.ReadLine();
+            StringBuilder fieldWithoutBombs = new StringBuilder();
+            int bombPower = 0;
+
+            for (int i = 0; i < bombField.Length; i++)
+            {
+                char currCharacter = bombField[i];
+                if (!(currCharacter == '>'))
+                {
+                    if (bombPower > 0)
+                    {
+                        bombPower--;
+                        continue;
+                    }
+                    else
+                    {
+                        fieldWithoutBombs.Append(currCharacter);
+                    }
+                }
+                else
+                {
+                    bombPower += bombField[i + 1] - 48;
+                    fieldWithoutBombs.Append(currCharacter);
+                    continue;
+                }
+            }
+
+            Console.WriteLine(fieldWithoutBombs.ToString());
+        }
+    }
+}
