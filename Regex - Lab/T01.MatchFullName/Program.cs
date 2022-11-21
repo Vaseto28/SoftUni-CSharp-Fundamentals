72a0a55f739d22f7c8336f5d618282f560dda540
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-21 13:31:33 +0200

    Completed task: T01.MatchFullName

---
+++ Regex - Lab/T01.MatchFullName/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T01.MatchFullName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\b[A-Z]{1}[a-z]+\s[A-Z]{1}[a-z]+\b";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            Console.WriteLine(String.Join(" ", matches));
+        }
+    }
+}
