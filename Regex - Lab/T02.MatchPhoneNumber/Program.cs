e3e8ec827be0e18c3ade81bbab607eda9ea2ee4b
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-21 13:31:48 +0200

    Completed task: T02.MatchPhoneNumber

---
+++ Regex - Lab/T02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T02.MatchPhoneNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359([\s|\-])2\1\d{3}\1\d{4}\b";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
