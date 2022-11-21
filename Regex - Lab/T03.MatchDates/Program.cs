a16281dad20de7a7c69d2f0eeb0d79aeb9302afe
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-21 13:32:30 +0200

    Completed task: T03.MatchDates

---
+++ Regex - Lab/T03.MatchDates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T03.MatchDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<day>\d{2})(?<separators>[\.\-\/])(?<month>[A-Z]{1}[a-z]{2})\2(?<year>\d{4})";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            foreach (Match match in matches)
+            {
+                string day = match.Groups["day"].Value;
+                string month = match.Groups["month"].Value;
+                string year = match.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
