e91aac6551051a99334b6f15d5319a420d39017f
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-21 13:34:01 +0200

    Completed task: T03.SoftuniBarIncome

---
+++ Regex - Exercise/T03.SoftuniBarIncome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace T03.SoftuniBarIncome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\%(?<costumer>[A-Z]{1}[a-z]+)\%[^\|\$\%\.]*?\<(?<product>\w+)\>[^\|\$\%\.]*?\|(?<quantity>\d+)\|[^\|\$\%\.]*?(?<price>\d+(\,\d+)?)\$";
+
+            decimal totalPrice = 0m;
+            string rawInput;
+            while ((rawInput = Console.ReadLine()) != "end of shift")
+            {
+                Match currMatch = Regex.Match(rawInput, pattern);
+
+                if (currMatch.Success)
+                {
+                    string costumer = currMatch.Groups["costumer"].Value;
+                    string product = currMatch.Groups["product"].Value;
+                    int qty = int.Parse(currMatch.Groups["quantity"].Value);
+                    decimal price = decimal.Parse(currMatch.Groups["price"].Value);
+
+                    decimal totalPriceForCurrCostumer = qty * price;
+                    totalPrice+=totalPriceForCurrCostumer;
+
+                    Console.WriteLine($"{costumer}: {product} - {totalPriceForCurrCostumer:f2}");
+                }
+            }
+
+            Console.WriteLine($"Total income: {totalPrice:f2}");
+        }
+    }
+}
