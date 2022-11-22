d8ce09146ed795d53d393a795c830d48473035f0
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-22 21:41:05 +0200

    Completed task: T02.MemoryGame

---
+++ Exams/01.FirstExamPreparation/T02.MemoryGame/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace T03.MemoryGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            string command;
+            var mooves = 0;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                //Getting the indexes
+                mooves++;
+                int[] indexes = command.Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                var firstIndex = indexes[0];
+                var secondIndex = indexes[1];
+
+                //Validating the indexes
+                if (firstIndex == secondIndex || firstIndex < 0 || firstIndex >= elements.Count || secondIndex < 0 || secondIndex >= elements.Count)
+                {
+                    elements.Insert(elements.Count / 2, $"-{mooves}a");
+                    elements.Insert(elements.Count / 2, $"-{mooves}a");
+                    Console.WriteLine("Invalid input! Adding additional elements to the board");
+                    continue;
+                }
+
+                //Getting the elements and see if they are matching or not
+                var firstElement = elements[firstIndex];
+                var secondElement = elements[secondIndex];
+                if (firstElement == secondElement)
+                {
+                    if (firstIndex > secondIndex)
+                    {
+                        elements.RemoveAt(firstIndex);
+                        elements.RemoveAt(secondIndex);
+                    }
+                    else
+                    {
+                        elements.RemoveAt(secondIndex);
+                        elements.RemoveAt(firstIndex);
+                    }
+                    Console.WriteLine($"Congrats! You have found matching elements - {firstElement}!");
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                }
+
+                //Checking if the player guessed all the elements before gigving the "end" command
+                if (elements.Count == 0)
+                {
+                    Console.WriteLine($"You have won in {mooves} turns!");
+                    return;
+                }
+            }
+
+            //Printing the messages for loses
+            Console.WriteLine("Sorry you lose :(");
+            Console.WriteLine(String.Join(" ", elements));
+        }
+    }
+}
