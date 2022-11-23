ff8e4ff4b2a688000a8f8d626e3439db9208c60d
Vasil Kremenarov <vaseto.k28@gmail.com>
2022-11-23 21:54:58 +0200

    Completed task: T01.SoftUniReception

---
+++ Exams/03.ThirdExamPreparation/T01.SoftUniReception/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace T01.SoftUniReception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstEmployerEfficency = int.Parse(Console.ReadLine());
+            var secondEmployerEfficency = int.Parse(Console.ReadLine());
+            var thirdEmployerEfficency = int.Parse(Console.ReadLine());
+            var studentsQuestionsCount = int.Parse(Console.ReadLine());
+
+            var allEmployersEfficency = firstEmployerEfficency + secondEmployerEfficency + thirdEmployerEfficency;
+            var hours = 0;
+            while (studentsQuestionsCount > 0)
+            {
+                hours++;
+                if (hours % 4 == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    studentsQuestionsCount -= allEmployersEfficency;
+                }
+            }
+
+            Console.WriteLine($"Time needed: {hours}h.");
+        }
+    }
+}
